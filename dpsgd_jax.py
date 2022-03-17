a3ec4399a5813c00858909e5ebd8e278ecb9e17e
Shadab Shaikh <sshadab84@gmail.com>
2022-03-16 22:49:21 -0700

    Make gradient clipping code work

--- dpsgd_jax.py
+++ dpsgd_jax.py
@@ -5,12 +5,13 @@ import numpy as np
 import jax.numpy as jnp
 from jax import grad, jit, vmap, value_and_grad, random
 
+from jax.tree_util import Partial 
+
 # Import some additional JAX and dataloader helpers
 from jax.scipy.special import logsumexp
 from jax.example_libraries import optimizers
 from jax.example_libraries import stax
-from jax.experimental.stax import (BatchNorm, Conv, Dense, Flatten,
-                                   Relu, LogSoftmax)
+from jax.example_libraries.stax import (BatchNorm, Conv, Dense, Flatten, Relu, LogSoftmax)
 
 # for privacy accounting
 from opacus.accountants import create_accountant
@@ -24,7 +25,47 @@ from torchvision import datasets, transforms
 import os
 os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "False"
 
-# HYPERPARAMS 
+def softmax_loss(model, params, batch):
+    inputs, targets = batch 
+    logits = model(params, inputs)
+    one_hot = jax.nn.one_hot(targets, logits.shape[-1])
+    logits = stax.logsoftmax(logits)  # log normalize
+    return -jnp.mean(jnp.sum(logits * one_hot, axis=-1))  # cross entropy loss
+
+def gradient_clipping(model, loss_fn, params, l2_norm_bound, single_example_batch):
+    """Compute gradient for a single example and clip its norm to 'l2_norm_bound' 
+    (Note the single example batch is automatically generated via vmap function"""
+
+    reshaped_input = jnp.expand_dims(single_example_batch[0], axis=1)
+    target = single_example_batch[1]
+    reshaped_batch = (reshaped_input, target)
+    grads = grad(loss_fn, argnums=1)(model, params, reshaped_batch)
+    nonempty_grads, tree_def = tree_flatten(grads)
+    total_grad_norm = jnp.linalg.norm([jnp.linalg.norm(neg.ravel()) for neg in nonempty_grads])
+    divisor = jnp.maximum(total_grad_norm / l2_norm_bound, 1.)
+    normalized_nonempty_grads = [g / divisor for g in nonempty_grads]
+    return tree_unflatten(tree_def, normalized_nonempty_grads)
+
+def compute_private_grad(model, loss_fn, params, batch, key, l2_norm_bound, noise_multiplier, batch_size):
+    """Return differentially private gradients for params, evaluated on batch."""
+
+    # vmap function
+    clipped_grads = vmap(gradient_clipping,
+                        in_axes=(None, None, None, None, 0),
+                        out_axes=0)(model, loss_fn, params, l2_norm_bound, batch)
+    clipped_grads_flat, grads_treedef = tree_flatten(clipped_grads)
+    aggregated_clipped_grads = [g.sum(0) for g in clipped_grads_flat]
+    keys = random.split(key, len(aggregated_clipped_grads))
+    noised_aggregated_clipped_grads = [
+        g + l2_norm_bound * noise_multiplier * random.normal(r, g.shape)
+        for r, g in zip(keys, aggregated_clipped_grads)
+    ]
+    normalized_noised_aggregated_clipped_grads = [
+        g / batch_size for g in noised_aggregated_clipped_grads
+    ]
+    return tree_unflatten(grads_treedef, normalized_noised_aggregated_clipped_grads)
+
+# HYPERPARAMS
 # Generate key which is used to generate random numbers
 key = random.PRNGKey(42)
 num_classes = 10
@@ -40,28 +81,9 @@ learning_rate = 0.01
 # accountant creation
 accountant = create_accountant("rdp")
 std_dev = 1.0
-l2_norm_clip = 1.0 # is equal to C in DPSGD paper
-noise_multiplier = std_dev / l2_norm_clip
+l2_norm_bound = 1.0 # is equal to C in DPSGD paper
+noise_multiplier = std_dev / l2_norm_bound
 step_size = 1e-3
-
-@jit
-# updating the params of the model
-def private_update(key, i, opt_state, batch):
-    params = get_params(opt_state)
-    key = random.fold_in(key, i)  # get new key for new random numbers
-    return opt_update(
-        i,
-        private_grad(
-            model,
-            softmax_loss, 
-            params, 
-            batch, 
-            key,
-            l2_norm_clip, 
-            noise_multiplier,
-            batch_size), 
-        opt_state
-    )
 
 # MNIST
 _MNIST_MEAN = [0.1307]
@@ -116,97 +138,45 @@ itercount = itertools.count()
 
 eps_arr = []
 
-def softmax_loss(model, params, batch):
-    inputs, targets = batch
-    logits = model(params, inputs)
-    one_hot = jax.nn.one_hot(targets, logits.shape[-1])
-    logits = stax.logsoftmax(logits)  # log normalize
-    return -jnp.sum(logits * one_hot, axis=-1)  # cross entropy loss
-
-# Loop over the training epochs
+# Main loop
 for epoch in range(num_epochs):
     start_time = time.time()
     for batch_idx, (inputs, targets) in enumerate(train_loader):
         i = next(itercount) # tracks the current step
+        key = random.fold_in(key, i)  # get new key for new random numbers
+
         inputs = jnp.array(inputs)
+        targets = jnp.array(targets)
         batch = (inputs, targets)
 
         # setting the loss function
-        
-        opt_state = private_update(model, loss, key, i, opt_state, batch)
+        clipped_noisy_grads = jit(compute_private_grad(model, 
+                                                       softmax_loss, 
+                                                       params, 
+                                                       batch,
+                                                       key, 
+                                                       l2_norm_bound, 
+                                                       noise_multiplier, 
+                                                       batch_size))
+        opt_state = opt_update(i, clipped_noisy_grads, opt_state)
         params = get_params(opt_state)
 
-def clipped_grad(model, loss, params, l2_norm_clip, single_example_batch):
-    """Evaluate gradient for a single-example batch and clip its grad norm."""
-    grads = grad(partial(loss, model))(params, single_example_batch)
-    nonempty_grads, tree_def = tree_flatten(grads)
-    total_grad_norm = jnp.linalg.norm([jnp.linalg.norm(neg.ravel()) for neg in nonempty_grads])
-    divisor = jnp.maximum(total_grad_norm / l2_norm_clip, 1.)
-    normalized_nonempty_grads = [g / divisor for g in nonempty_grads]
-    return tree_unflatten(tree_def, normalized_nonempty_grads)
-
-def private_grad(model, loss, params, batch, key, l2_norm_clip, noise_multiplier, batch_size):
-
-    """Return differentially private gradients for params, evaluated on batch."""
-    clipped_grads = vmap(partial(clipped_grad, model, loss), (None, None, 0))(params, l2_norm_clip,
-                                                                              batch)
-    clipped_grads_flat, grads_treedef = tree_flatten(clipped_grads)
-    aggregated_clipped_grads = [g.sum(0) for g in clipped_grads_flat]
-    keys = random.split(key, len(aggregated_clipped_grads))
-    noised_aggregated_clipped_grads = [
-        g + l2_norm_clip * noise_multiplier * random.normal(r, g.shape)
-        for r, g in zip(keys, aggregated_clipped_grads)
-    ]
-    normalized_noised_aggregated_clipped_grads = [
-        g / batch_size for g in noised_aggregated_clipped_grads
-    ]
-    return tree_unflatten(grads_treedef, normalized_noised_aggregated_clipped_grads)
+        # take a step for acountant and measure privacy spent
+        accountant.step(noise_multiplier=noise_multiplier, sample_rate=sampling_prob)
+        eps_till_now, best_alpha = accountant.get_privacy_spent(delta=delta)
+        eps_arr.append(eps_till_now)
 
 
+eps_arr = main(num_epochs, opt_state)
+x = np.arange(num_epochs)
+plt.plot(x, eps_arr)
+plt.savefig("eps vs epochs")
+print("Epoch {} | T: {:0.2f} | eps till now: {:0.2f}".format(
+                                                            epoch+1,
+                                                            epoch_time,
+                                                            eps_till_now))
 
-# def main(num_epochs, opt_state):
-#     """ Implements a learning loop over epochs. """
-#     # Initialize placeholder for loggin
-#     log_acc_train, log_acc_test, train_loss = [], [], []
-
-#     # Get the initial set of parameters
-#     params = get_params(opt_state)
-#     itercount = itertools.count()
-
-#     eps_arr = []
-
-#     # Loop over the training epochs
-#     for epoch in range(num_epochs):
-#         start_time = time.time()
-#         for batch_idx, (data, target) in enumerate(train_loader):
-#             i = next(itercount) # tracks the current step
-#             x = jnp.array(data)
-#             y = one_hot(jnp.array(target), num_classes)
-#             batch = (x, y)
-#             # params, opt_state, loss = private_update(key, i, opt_state, params, x, y)
-#             opt_state = private_update(key, i, opt_state, batch)
-#             params = get_params(opt_state)
-
-#             # take a step in acountant
-#             accountant.step(noise_multiplier=noise_multiplier, sample_rate=sampling_prob)
-
-#         eps_till_now, best_alpha = accountant.get_privacy_spent(delta=delta)
-#         eps_arr.append(eps_till_now)
-
-#         epoch_time = time.time() - start_time
-#         # train_acc = accuracy(params, train_loader)
-#         # test_acc = accuracy(params, test_loader)
-#         # log_acc_train.append(train_acc)
-#         # log_acc_test.append(test_acc)
-#         print("Epoch {} | T: {:0.2f} | eps till now: {:0.2f}".format(
-#                                                                     epoch+1,
-#                                                                     epoch_time,
-#                                                                     eps_till_now))
-
-#     # return train_loss, log_acc_train, log_acc_test
-#     return eps_arr
-
-# eps_arr = main(num_epochs, opt_state)
-# x = np.arange(num_epochs)
-# plt.plot(x, eps_arr)
-# plt.savefig("eps vs epochs")
+# LIST OF PLOTS:
+# - (unclipped) gradient plots
+# - eps vs iterations/steps
+# - alpha order RDP
